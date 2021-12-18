104dcee817a795b47ee58326e75c8e63c680d9b8
DonaldWu <donaldwu1101@gmail.com>
2021-12-18 22:05:47 +0800

    Reset Package.swift for playground

--- ContentView.swift
+++ ContentView.swift
@@ -3,6 +3,7 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
+            Text("Test")
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
